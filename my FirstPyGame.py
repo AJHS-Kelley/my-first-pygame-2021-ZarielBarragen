cdf301fa7355aa5e7c25721ad24584a759208fdd
Isaiah R <90346468+ZarielBarragen@users.noreply.github.com>
2021-11-29 14:42:18 -0500

    4A v0.3
    Added font and text

--- my FirstPyGame.py
+++ my FirstPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Isaiah Reyes, 11/29/21  2:25pm, v0.1
+# My First PyGame, Isaiah Reyes, 11/29/21  2:25pm, v0.2
 
 import pygame, sys
 from pygame.locals import *
@@ -8,7 +8,7 @@ pygame.init()
 
 # Setup our window 
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Hello World')
+pygame.display.set_caption('Hello World!')
 
 #Setup Colors
 BLACK = (0, 0, 0,)
@@ -16,3 +16,12 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+#Setup Font
+basicFont = pygame.font.SysFont(None, 48)
+
+#Setup Text
+text = basicFont.render('Hello World!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.cenerx = windowsSurface.get_rect().centerx
+textRect.cenerx = windowsSurface.get_rect().centery
