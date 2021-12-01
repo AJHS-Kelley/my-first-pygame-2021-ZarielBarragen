2a26e3a71ba8249b1c2985de5f266287aa7f6d50
Isaiah R <90346468+ZarielBarragen@users.noreply.github.com>
2021-12-01 13:55:40 -0500

    4A v0.6
    created draw lines.

--- my FirstPyGame.py
+++ my FirstPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Isaiah Reyes, 12/1/21  1:40pm, v0.5
+# My First PyGame, Isaiah Reyes, 12/1/21  1:55pm, v0.6
 
 import pygame, sys
 from pygame.locals import *
@@ -10,18 +10,19 @@ pygame.init()
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('Hello World!')
 
-#Setup Colors
+# Setup Colors
 BLACK = (0, 0, 0,)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 CRIMSON = (144, 0, 37)
+DARKARTS = (49, 0, 92)
 
-#Setup Font
+# Setup Font
 basicFont = pygame.font.SysFont(None, 48)
 
-#Setup Text
+# Setup Text
 text = basicFont.render('Hello World!', True, WHITE, BLUE)
 textRect = text.get_rect()
 textRect.cenerx = windowsSurface.get_rect().centerx
@@ -31,5 +32,9 @@ textRect.cenerx = windowsSurface.get_rect().centery
 windowSurface.fill(CRIMSON) 
 
 # Draw a polygon onto the screen
-pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56,277), (0, 106))
+pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56,277), (0, 106)))
 
+# Draw lines on the screen
+pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
+pygame.draw.line(windowSurface, BLACK, (75, 60), (120, 75), 2)
+pygame.draw.line(windowSurface, DARKARTS, (0, 150), (150, 0), 1)
