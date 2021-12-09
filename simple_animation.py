38248184c068e7090f999c07724b5c8ade023e04
Isaiah R <90346468+ZarielBarragen@users.noreply.github.com>
2021-12-09 14:25:48 -0500

    [4A] v0.4
    Created box values

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with Pygame, Isaiah Reyes, 12/09/21, 2:16, v0.3
+# Simple Animation with Pygame, Isaiah Reyes, 12/09/21, 2:25, v0.4
 
 import pygame, sys, time
 from pygame.locals import *
@@ -28,3 +28,9 @@ BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 NIGHTSKY = (77, 0, 118)
 DARKERBLOOD = (70, 0, 0)
+
+# setup box data
+b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':DARKERBLOOD, 'dir':UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':NIGHTSKY, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLACK, 'dir':DOWNLEFT}
+boxes = [b1, b2, b3]
