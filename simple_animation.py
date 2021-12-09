1790361e37420b7be27cd645cff35d74ad4d2486
Isaiah R <90346468+ZarielBarragen@users.noreply.github.com>
2021-12-09 14:07:12 -0500

    [4A] v0.1
    Imported modules for pygame system and time. Initialized pygame module and setup window

--- simple_animation.py
+++ simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with Pygame, Isaiah Reyes, 12/09/21, 12/9/21, v0.0
+# Simple Animation with Pygame, Isaiah Reyes, 12/09/21, 2:06, v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup pygame
+pyagame.init()
+
+#Setup the window 
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
