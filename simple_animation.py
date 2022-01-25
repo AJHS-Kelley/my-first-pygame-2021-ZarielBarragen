d5bc4157ccb27b5f0a57a12aa1fc2b0861a7c159
Isaiah R <s11670916@students.duvalschools.org>
2022-01-25 14:42:50 -0500

    [4A] v.07
    Created the code to check for the boxes leaving the window

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with Pygame, Isaiah Reyes, 12/09/21, 2:46, v0.6
+# Simple Animation with Pygame, Isaiah Reyes, 1/25/22, 2:42, v0.7
 
 import pygame, sys, time
 from pygame.locals import *
@@ -43,19 +43,44 @@ while True:
             pygame.quit()
             sys.exit()
 
-windowSurface.fill(WHITE)
+    windowSurface.fill(WHITE)
 
-for b in boxes:
+    for b in boxes:
     # Move the box data structure 
-    if b['dir'] == DOWNLEFT:
-        b['rect'].left -= MOVESPEED
-        b['rect'].top += MOVESPEED
-    if b['dir'] == DOWNRIGHT:
-        b['rect'].left += MOVESPEED
-        b['rect'].top += MOVESPEED
-    if b['dir'] == UPLEFT:
-        b['rect'].left -= MOVESPEED
-        b['rect'].top -= MOVESPEED
-    if b['dir'] == UPRIGHT:
-        b['rect'].left += MOVESPEED
-        b['rect'].top -= MOVESPEED
+        if b['dir'] == DOWNLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top += MOVESPEED
+        if b['dir'] == DOWNRIGHT:
+            b['rect'].left += MOVESPEED
+            b['rect'].top += MOVESPEED
+        if b['dir'] == UPLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top -= MOVESPEED
+        if b['dir'] == UPRIGHT:
+            b['rect'].left += MOVESPEED
+            b['rect'].top -= MOVESPEED
+
+        if b['rect'].top < 0:
+            # The box had moved past the top
+            if b['dir'] == UPLEFT:
+                b['dir'] = DOWNLEFT
+            if b['dir'] == UPRIGHT:
+                b['dir'] = DOWNRIGHT
+            if b['rect'].bottom > WINDOWHEIGHT:
+                # The box has moved past the bottom
+                if b['dir'] == DOWNLEFT:
+                    b['dir'] = UPLEFT
+                if b['dir'] == DOWNRIGHT:
+                    b['dir'] = UPRIGHT
+            if b['rect'].left < 0:
+                # The box has moved past the left.
+                if b['dir'] == DOWNLEFT:
+                    b['dir'] = UPLEFT
+                if b['dir'] == DOWNRIGHT:
+                    b['dir'] = UPRIGHT
+            if b['rect'].right > WINDOWIDTH:
+                # The box has moved past the right
+                if b['dir'] == DOWNRIGHT:
+                    b['dir'] = DOWNLEFT
+                if b['dir'] == UPLEFT:
+                    b['dir'] = UPRIGHT
